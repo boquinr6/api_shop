4d41352c1a4c03cc5e2362c4752cac097d8aad22
Raul Boquin <boquinr6>
2025-04-20 18:27:44 -0400

    [RB] create Inventory Model and initial read/write methods and added specs

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,25 @@
 class Inventory < ApplicationRecord
+	# ensure items in Inventory have unique codes
+	validates :code, presence: true, uniqueness: true
+
+
+
+	def self.get_all_data
+		Inventory.all
+	end
+
+	def self.find_by_code(code)
+		Inventory.where(code:).first
+	end
+
+	def update_price(new_price)
+		begin
+
+			update(price: Float(new_price))
+
+		rescue 
+			raise ArgumentError, "Price '#{new_price}' must be numerical"	 
+		end
+	end
+
 end
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
+
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let!(:inventory) do
+    Inventory.create(
+      code: "test_code", 
+      name: "test_name",
+      price: 1.23
+      )
+  end
+
+  describe "#update_price" do
+    subject do 
+      inventory.update_price("456.123")
+    end
+
+    it "" do
+      expect(subject).to be_truthy
+      
+    end
+
+    it "" do
+      subject
+      expect(inventory.price).to eq(456.123)
+    end
+  end
 end
